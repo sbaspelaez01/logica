ef2d233ec587c63c9fb6442a14d18fddeb417c5d
sbaspelaez01 <sbastianpelaez09@gmail.com>
2022-10-07 19:08:10 -0500

    fibo terminado

--- Fibonacci/fibo.js
+++ Fibonacci/fibo.js
@@ -1,5 +1,17 @@
-let ciclo = 1
+let result = [1,2]
 
-function inicio (){
-   
+
+function inicio() {
+let valor = document.querySelector('.value').value
+document.querySelector('.value').value =''
+// alert (valor)
+
+// let result = [0,1]
+for (let i = 0; i < valor-1; i++) {
+    let insertar =result[i]+result[i+1]
+    result.push(insertar)
+    // alert(insertar)
+    }
+
+document.querySelector('.table').innerHTML = `<tr><td>Ciclo = ${result}</td></tr>`
 }
